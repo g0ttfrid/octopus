c2ecdbc621e092be8bb14ce167238efb83f11bee
g0ttfrid <yurimaia@protonmail.ch>
2024-07-01 00:27:37 -0300

    Update getlinks.py

--- modules/getlinks.py
+++ modules/getlinks.py
@@ -2,6 +2,7 @@ import requests
 import lxml.html
 import urllib3
 import argparse
+from fake_useragent import UserAgent
 from urllib.parse import urlparse
 
 urllib3.disable_warnings()
@@ -23,13 +24,21 @@ def newsubs(list):
     return data
 
 
-def crawling(subdomains):
+def crawling(subdomains, proxy=None):
     print('[+] Crawling')
+
+    if proxy:
+        proxies = {'http': f'http://{proxy}', 'https': f'http://{proxy}'}
+    else:
+        proxies = None
     
     links = set()
     for sub in subdomains:
+        ua = UserAgent()
+        headers = {'User-Agent': f'{ua.random}'}
+        
         try:
-            r = requests.get('https://{s}'.format(s=sub), timeout=(5,5), verify=False)
+            r = requests.get('https://{s}'.format(s=sub), headers=headers, proxies=proxies, timeout=8, verify=False, allow_redirects=False)
             dom = lxml.html.fromstring(r.content)
             links.add('https://{s}'.format(s=sub))
             for link in dom.xpath('//script/@src'):
@@ -51,10 +60,10 @@ def crawling(subdomains):
         return None
 
 
-def getlinks(subdomains, urls):
+def getlinks(subdomains, urls, proxy=None):
     data = set()
     subdomains.update(newsubs(urls))
-    data = crawling(subdomains)
+    data = crawling(subdomains, proxy)
     data.update(urls)
     return data
 
